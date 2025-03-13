f4c1b6fce7ffb4c1949928200fd3e51a981873de
user.walid-ka <user.walid.kouider-ayad@heptacom.de>
2025-03-13 15:44:53 +0100

    registration title changed

--- register.php
+++ register.php
@@ -141,7 +141,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Registrations</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
